8f4d6fe2544abe0aebbd48b941a0d24c1a2def86
Ravi <rssharmakapro@gmail.com>
2022-11-03 16:57:39 +0530

    changes again

--- ravi_portfolio/src/Components/About.jsx
+++ ravi_portfolio/src/Components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
         <Box>
           <Image src='https://static.vecteezy.com/system/resources/previews/003/795/478/original/cartoon-man-working-free-vector.jpg' alt='Ravi Sharma' className='About_image' boxSize={['xs', 'md', 'lg', '4xl']}/>
         </Box>
-        <Box width='auto' margin='auto' padding='20px' marginTop='0px'>
+        <Box width='50%' margin='auto' padding='20px' marginTop='0px'>
         <Heading as='h1' textAlign='center'>About Me</Heading>
         <Text padding='20px'>
             An enthusiastic Full Stack Developer. Capable of writing production-ready code using ReactJS, Redux, and CSS on the front-end, NodeJS, and Express on the backend to build single-page applications. Passionate about coding and strongly interested in working in a product-based company.
